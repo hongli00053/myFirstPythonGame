e7a79a84b2e1c991fbfa154e48336b427742a595
rhlamy1992@gmail.com <rhlamy1992@gmail.com>
2024-12-23 18:31:13 -0500

    Created a hit method to detect and measure collisions between two objects.

--- amy_pygame.py
+++ amy_pygame.py
@@ -31,6 +31,7 @@ class player(object):
         self.walkCount = 0
         self.jumpCount = 10
         self.standing = True
+        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
         
     def draw(self,win):
         if self.walkCount + 1 >= 27:
@@ -47,7 +48,10 @@ class player(object):
                 win.blit(walkRight[0], (self.x, self.y))
             else:
                 win.blit(walkLeft[0], (self.x, self.y))
-
+        self.hitbox = (self.x + 17, self.y + 11, 29, 52)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+        
+        
 class projectile(object):
     def __init__(self, x, y, radius, color,facing):
         self.x = x
@@ -81,6 +85,10 @@ class enemy(object):
         self.path = (x,end)
         self.walkCount = 0
         self.vel = 3
+        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+        
+        
+        
     def draw(self, win):
         self.move()
         if self.walkCount +1 >= 33:
@@ -91,7 +99,8 @@ class enemy(object):
         else:
             win.blit(self.walkLeft[self.walkCount //3], (self.x, self.y))
             self.walkCount += 1
-            
+        self.hitbox = (self.x + 17, self.y + 2, 31, 57)
+        pygame.draw.rect(win, (255,0, 0,), self.hitbox, 2)
         
     def move(self):
         if self.vel >0:
@@ -107,7 +116,8 @@ class enemy(object):
                 self.vel = self.vel *-1
                 self.walkCount = 0
         
-    
+    def hit(self):
+        print('hit')
     
     
     
@@ -124,23 +134,34 @@ def redrawGameWindow():
 # mainloop
 man = player(300, 410, 64, 64)
 zombies = enemy(100, 410, 64, 64, 450)
+shootloop = 0
 bullets = []
 run = True
 while run:
     clock.tick(27)
+    
+    if shootloop > 0:
+        shootloop +=1
+    if shootloop > 3:
+        shootloop = 0
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     for bullet in bullets:
+        if bullet.y - bullet.radius < zombies.hitbox[1] + zombies.hitbox[3] and bullet.y + bullet.radius > zombies.hitbox[1]:
+            if bullet.x + bullet.radius > zombies.hitbox[0] and bullet.x -bullet.radius <zombies.hitbox[0] + zombies.hitbox[2]:
+                zombies.hit()
+                bullets.pop(bullets.index(bullet))
+         
         if bullet.x < 500 and bullet.x > 0:
             bullet.x += bullet.vel
         else:
             bullets.pop(bullets.index(bullet))
                 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE] and shootloop ==0:
         if man.left:
             facing = -1
         else:
@@ -148,7 +169,8 @@ while run:
             
         if len(bullets) < 5:
             bullets.append(projectile(round(man.x + man.width//2), round(man.y + man.height//2), 6, (0, 0,0), facing))
-
+        shootloop = 1
+        
     if keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
         man.left = True
